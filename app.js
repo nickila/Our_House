22ed0ef843c42d4f5f449952317652d55210689b
Brian Nickila <brian.nickila@gmail.com>
2018-11-03 12:54:17 -0500

    Added javascript/jquery basic to take input from user and set it as variable.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // JavaScript file for Project#1
+// var street;
+// var city;
+// var state;
+// var zip;
+
+
+$(".btn").on("click", function(event) {
+    event.preventDefault();
+    var street = $("#street").val().trim();
+    var city = $("#city").val().trim();
+    var state = $("#state").val().trim();
+    var zip = $("#zip").val().trim();
+    console.log(street, city, state, zip);
+})
+
